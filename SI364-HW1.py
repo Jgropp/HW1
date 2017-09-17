369be6b6bd7de9bf4199e545f8d064fc4987937d
Jonathan Gropper <JonnyGropper@Jonnys-MacBook-Pro.local>
2017-09-17 16:21:08 -0400

    HW1

--- SI364-HW1.py
+++ SI364-HW1.py
@@ -1,3 +1,4 @@
+
 ## HW 1
 ## SI 364 F17
 ## Due: September 19, 2017
@@ -18,24 +19,34 @@ from flask import Flask
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+@app.route('/class')
 def hello_to_you():
-    return 'Hello!'
-
-
-if __name__ == '__main__':
-    app.run()
-
+    return 'Welcome to SI 364'
 
 ## [PROBLEM 2] - 250 points
-## Edit the code chunk above again so that if you go to the URL 'http://localhost:5000/movie/<name-of-movie-here-one-word>' you see a big dictionary of data on the page. For example, if you go to the URL 'http://localhost:5000/movie/ratatouille', you should seesomething like the data shown in the included file sample_ratatouille_data.txt, which contains data about the animated movie Ratatouille. However, if you go to the url http://localhost:5000/movie/titanic, you should get different data, and if you go to the url 'http://localhost:5000/movie/dsagdsgskfsl' for example, you should see data on the page that looks like this:
+## Edit the code chunk above again so that if you go to the URL 'http://localhost:5000/movie/<name-of-movie-here-one-word>' 
+# you see a big dictionary of data on the page. For example, if you go to the URL 'http://localhost:5000/movie/ratatouille', 
+# you should seesomething like the data shown in the included file sample_ratatouille_data.txt, 
+# which contains data about the animated movie Ratatouille. However, if you go to the url http://localhost:5000/movie/titanic, 
+# you should get different data, and if you go to the url 'http://localhost:5000/movie/dsagdsgskfsl'
+ # for example, you should see data on the page that looks like this:
+import requests 
+import json 
+
+@app.route('/movie/<movietitle>')
+def movie(movietitle):
+	p = {'term': movietitle, "media": "movie"}
+	url = requests.get('http://itunes.apple.com/search?', params = p)
+	return url.text 
+
+if __name__ == '__main__':
+	app.run()
+
 
 # {
 #  "resultCount":0,
 #  "results": []
 # }
-
-
 ## You should use the iTunes Search API to get that data.
 ## Docs for that API are here: https://affiliate.itunes.apple.com/resources/documentation/itunes-store-web-service-search-api/
 ## Of course, you'll also need the requests library and knowledge of how to make a request to a REST API for data.
